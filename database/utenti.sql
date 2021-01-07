724f33c90ba7a91e71b6a4947f3ab5619212d659
GitDeepak-C <61829381+GitDeepak-C@users.noreply.github.com>
2021-01-07 23:05:09 +0100

    Update

---
+++ database/utenti.sql
@@ -0,0 +1,108 @@
+[{
+	"username": "Alfio.Alfio",
+	"name": "Alfio",
+	"surname": "Alfio",
+	"telefono" : 3333333333,
+	"email": "alfio.alfio@gmail.com",
+	"dataNascita": { "$date": "2000-04-03T00:00:00+01:00"},						
+	"indirizzo": "Via giovanni piumati 52, Bra"
+},
+{
+	"username": "Beatrice.Bea",
+	"name": "Beatrice",
+	"surname": "Bea",
+	"telefono" : 3333333333,
+	"email": "Beatrice.bea@gmail.com",
+	"dataNascita": { "$date": "2000-12-13T13:35:00+01:00"},
+	"indirizzo": "Via cesare battisti 52, Torino"
+},
+{
+	"username": "Carlo.carletto",
+	"name": "Carlo",
+	"surname": "carletto",
+	"telefono" : 3333333333,
+	"email": "Carlo.carletto@gmail.com",
+	"dataNascita": { "$date": "2004-06-01T00:00:00+01:00"},
+	"indirizzo": "Via giovanni piumati 52, Bra"
+},
+{
+	"username": "Flaminia.fea",
+	"name": "Flaminia",
+	"surname": "fea",
+	"telefono" : 3333333333,
+	"email": "Flaminia.fea@gmail.com",
+	"dataNascita": { "$date": "1999-04-16T00:00:00.000+01:00"},
+	"indirizzo": "Via umberto 52, Bra"
+},
+{
+	"username": "Daniela.Pietoso",
+	"name": "Daniela",
+	"surname": "Pietoso",
+	"telefono" : 3333333333,
+	"email": "Daniela.Pietoso@gmail.com",
+	"dataNascita": { "$date": "2000-07-10T00:00:00Z"},
+	"indirizzo": "Via giovanni piumati 52, Bra"
+},
+{
+	"username": "Eraldo.mario",
+	"name": "Eraldo",
+	"surname": "mario",
+	"telefono" : 3333333333,
+	"email": "Eraldo.mario@gmail.com",
+	"dataNascita": { "$date": "2000-02-29T00:00:00+01:00"},
+	"indirizzo": "Via giovanni piumati 52, Bra"
+},
+{
+	"username": "Gustavo.lima",
+	"name": "Gustavo",
+	"surname": "lima",
+	"telefono" : 3333333333,
+	"email": "Gustavo.lima@gmail.com",
+	"dataNascita": { "$date": "1999-08-24T00:00:00Z"},
+	"indirizzo": "Via giovanni piumati 52, Bra"
+},
+{
+	"username": "Irma.alba",
+	"name": "Irma",
+	"surname": "alba",
+	"telefono" : 3333333333,
+	"email": "Irma.alba@gmail.com",
+	"dataNascita": { "$date": "1999-06-22T00:00:00+01:00"},
+	"indirizzo": "Via giovanni piumati 52, Alba"
+},
+{
+	"username": "Leonardo.bonucci",
+	"name": "Leonardo",
+	"surname": "Bonucci",
+	"telefono" : 3333333333,
+	"email": "Leonardo.bonucci@gmail.com",
+	"dataNascita": { "$date": "1998-11-13T00:00:00-01:00"},
+	"indirizzo": "Via giovanni piumati 52, Bra"
+},
+{
+	"username": "Marta.maremma",
+	"name": "Marta",
+	"surname": "maremma",
+	"telefono" : 3333333333,
+	"email": "Marta.maremma@gmail.com",
+	"dataNascita": { "$date":"1998-05-20T00:00:00Z"},
+	"indirizzo": "Via giovanni piumati 52, Carmagnola"
+},
+{
+	"username": "Nicola.penoso",
+	"name": "Nicola",
+	"surname": "Penoso",
+	"telefono" : 3333333333,
+	"email": "Nicola.penoso@gmail.com",
+	"dataNascita": { "$date": "1998-05-20T00:00:00Z"},
+	"indirizzo": "Via giovanni piumati 52, Cervere"
+},
+{
+	"username": "Olga.ollare",
+	"name": "Olga",
+	"surname": "ollare",
+	"telefono" : 3333333333,
+	"email": "Olga.ollare@gmail.com",
+	"dataNascita": { "$date": "2004-05-20T00:00:00+01:00"},
+	"indirizzo": "Via giovanni piumati 52, Alba"
+}]
