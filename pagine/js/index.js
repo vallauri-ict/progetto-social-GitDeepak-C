6e4b68b7f3950f6e88428ae0bd69b9ef4d45243a
GitDeepak-C <d.chopra.0505@vallauri.edu>
2021-01-15 14:15:35 +0100

    SignUp page modified

--- pagine/js/index.js
+++ pagine/js/index.js
@@ -13,7 +13,7 @@ $(document).ready(function (){
         let req = inviaRichiesta("POST", "/api/logout");
         req.fail(errore);
         req.done(function(data){
-
+            console.log(data);
         })
     });
 });
